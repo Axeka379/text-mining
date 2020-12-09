190568204344eb004b0f369843e72877df72237a
Axel Karlsson <axeka379@student.liu.se>
2020-12-09 08:53:18 +0100

    Added functions to clean tweet-text

--- PLTweetClassifier.py
+++ PLTweetClassifier.py
@@ -1,8 +1,57 @@
 import pandas as pd
 import bz2
+import matplotlib.pyplot as plt
+import re
+import string
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 df = pd.read_csv("modified_tweets.csv")
-       
+
+#Global
+stop_words = set(stopwords.words('english'))
+
+def remove_emojis(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  
+        u"\U0001F300-\U0001F5FF"  
+        u"\U0001F680-\U0001F6FF"  
+        u"\U0001F1E0-\U0001F1FF"  
+        u"\U00002500-\U00002BEF"  
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  
+        u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
+
+def clean_tweet(text):
+    text.lower()
+    text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)
+    text = re.sub(r'\@\w+|\#','', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = remove_emojis(text)
+    
+    text_tokens = word_tokenize(text)
+    filtered_words = [w for w in text_tokens if not w in stop_words]
+
+    return " ".join(filtered_words)
+
+
+
+
 print(df.size)
 
 '''
@@ -12,12 +61,18 @@ for index, row in df1.iterrows():
         #print(index, row['text']) 
 '''
 
-print(df)
+#print(df)
 #test_df = df.groupby('partition_1').value_counts()
-test_df = df['file_name'].value_counts()
+#test_df = df['file_name'].value_counts().plot(kind='bar', rot=0)
+#plt.show()
+print(df.iloc[0]['text'])
+print(clean_tweet(df.iloc[0]['text']))
 
-print(test_df)
+#for index, row in df.iterrows():
+#    print(index, row['text'])
+
+    
 
 
-#print(result.size)
+
 
--- merge_datasets.py
+++ merge_datasets.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import bz2
+import matplotlib.pyplot as plt
 
 df1 = pd.read_csv("2020-07-09_from_2020-09-19.csv")
 df2 = pd.read_csv("2020-09-20_from_2020-10-13.csv")
@@ -8,12 +9,22 @@ frames = [df1, df2]
 
 result_df = pd.concat(frames)
 
-#Regex out?
-print("First dataset", result_df.size)
-new_df = result_df[result_df['text'].str.contains(r'\bVAR\b|\bvar\b|\bVar\b')]
+print(result_df['file_name'].value_counts())
 
-print("Only the ones mentioning VAR", new_df.size)
+#Regex out?
+#print("First dataset", result_df.size)
+new_df = result_df[result_df['text'].str.contains(r'\bVAR\b|\bvar\b|\bVar\b|\bvideo assistant referee\b')]
+
+print(new_df['file_name'].value_counts())
+
+
+#print("Only the ones mentioning VAR", new_df.size)
 new_df = new_df[~new_df['text'].str.startswith('RT')]
+
+print(new_df['file_name'].value_counts().plot(kind='bar', rot=0))
+print(new_df.shape)
+'''
 
 print("No RT", new_df.size)
 new_df.to_csv('modified_tweets.csv', index=False)
+'''
